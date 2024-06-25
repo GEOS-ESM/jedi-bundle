c4f3cd7990910792f6a829e62297bd3cda19823f
asewnath <akira.sewnath@gmail.com>
2024-06-25 16:37:22 -0400

    adding comment

--- src/jedi_bundle/tests/test_pinned_versions.py
+++ src/jedi_bundle/tests/test_pinned_versions.py
@@ -2,6 +2,11 @@ import sys
 from jedi_bundle.utils.logger import Logger
 from jedi_bundle.utils.yaml import load_yaml
 
+'''
+    Script to search for 'pinned_versions' keyword in build.yaml
+    during github action test.
+'''
+
 logger = Logger('Pinned Versions Test')
 build_yaml_path = sys.argv[1]
 build_dict = load_yaml(logger, build_yaml_path)
