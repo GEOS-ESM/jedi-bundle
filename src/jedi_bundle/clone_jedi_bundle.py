133bfa38c3a30ed2a443df53ce3ce3d652402264
dooruk <dorukhan.ardag@nasa.gov>
2024-07-30 11:10:53 -0400

    GSW before VADER

--- src/jedi_bundle/clone_jedi_bundle.py
+++ src/jedi_bundle/clone_jedi_bundle.py
@@ -228,12 +228,12 @@ def clone_jedi(logger, clone_config):
                         f'If it\'s not a module it will be cloned at configure time.')
         elif repo == 'fv3':
             logger.info('Cloning fv3-interface.cmake from the jedi-bundle repo for fv3')
-            found, url, branch, \
+            found, url_tmp, branch_tmp, \
                 is_tag, is_commit = get_url_and_branch(logger, github_orgs, 'jedi-bundle',
                                                        default_branch, user_branch,
                                                        False, False)
 
-            clone_git_file(logger, url, ['fv3-interface.cmake'], path_to_source, depth=1)
+            clone_git_file(logger, url_tmp, ['fv3-interface.cmake'], path_to_source, depth=1)
             logger.info('Cloning fv3.')
             clone_git_repo(logger, url, branch,
                            os.path.join(path_to_source, repo), is_tag, is_commit)
